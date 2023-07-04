3ead2f2c8ee30807d29ce12b23d3c7ed2a5fa287
Fascinated <admin@fascinated.cc>
2023-07-04 17:25:08 +0100

    add msg for restarting php

--- docker/start.sh
+++ docker/start.sh
@@ -13,13 +13,15 @@ fi
 # Start php dep
 echo "Starting PHP"
 service php8.1-fpm start
-chmod 777 /run/php/php8.1-fpm.sock # I don't know how to fix this properly, but it works.
+# I don't know how to fix this properly, but it works.
+chmod 777 /run/php/php8.1-fpm.sock
 
 echo "Setting max upload size to ${MAX_UPLOAD_SIZE}"
 sed -i "s/^upload_max_filesize = .*/upload_max_filesize = ${MAX_UPLOAD_SIZE}/" /etc/php/8.1/fpm/php.ini
 sed -i "s/^post_max_size = .*/post_max_size = ${MAX_UPLOAD_SIZE}/" /etc/php/8.1/fpm/php.ini
 
 # Restart php to apply changes
+echo "Restarting PHP to apply changes for max upload size"
 service php8.1-fpm restart
 
 # Start Nginx
