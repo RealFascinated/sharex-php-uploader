5584bad1aa7c921ceeeaaa158e1ea786b2505443
Fascinated <admin@fascinated.cc>
2023-07-05 16:57:12 +0100

    bump ubuntu version

--- docker/start.sh
+++ docker/start.sh
@@ -12,6 +12,8 @@ fi
 
 # Letting php know that we are running in docker
 echo "env[DOCKER] = true" >> /etc/php/8.1/fpm/pool.d/www.conf
+echo "env[UPLOAD_SECRETS] = ${UPLOAD_SECRETS}" >> /etc/php/8.1/fpm/pool.d/www.conf
+
 echo "env[UPLOAD_DIR] = ${UPLOAD_DIR}" >> /etc/php/8.1/fpm/pool.d/www.conf
 echo "env[USE_RANDOM_FILE_NAMES] = ${USE_RANDOM_FILE_NAMES}" >> /etc/php/8.1/fpm/pool.d/www.conf
 echo "env[FILE_NAME_LENGTH] = ${FILE_NAME_LENGTH}" >> /etc/php/8.1/fpm/pool.d/www.conf
@@ -34,9 +36,6 @@ sed -i "s/^post_max_size = .*/post_max_size = ${MAX_UPLOAD_SIZE}/" /etc/php/8.1/
 
 # Set max upload size for nginx
 sed -i "s/client_max_body_size 500M;/client_max_body_size ${MAX_UPLOAD_SIZE};/" /etc/nginx/nginx.conf
-
-# Setting env variable in php-fpm (holy shit this is dumb.. why is php so shit?)
-echo "env[UPLOAD_SECRETS] = ${UPLOAD_SECRETS}" >> /etc/php/8.1/fpm/pool.d/www.conf
 
 # Restart php to apply changes
 echo "Restarting PHP to apply changes for max upload size"
